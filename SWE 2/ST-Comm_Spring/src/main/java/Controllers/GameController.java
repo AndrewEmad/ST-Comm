b2886f9ea42e0ded8752a2737d44c43f97b16f8d
Ahmed-Hussein-Karam <ahkcsit@gmail.com>
2017-04-22 22:39:42 +0200

    Template ready

--- SWE 2/ST-Comm_Spring/src/main/java/Controllers/GameController.java
+++ SWE 2/ST-Comm_Spring/src/main/java/Controllers/GameController.java
@@ -6,7 +6,6 @@ import java.util.Vector;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
