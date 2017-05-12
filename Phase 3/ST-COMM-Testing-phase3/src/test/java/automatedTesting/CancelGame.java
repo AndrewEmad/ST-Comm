18772ee2c571477a9787de100f185ae1d7e084a7
mariam ashraf <mariam ashraf>
2017-05-12 14:16:14 +0200

    Testing CopyGame and CancelGame

---
+++ Phase 3/ST-COMM-Testing-phase3/src/test/java/automatedTesting/CancelGame.java
@@ -0,0 +1,68 @@
+package automatedTesting;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class CancelGame 
+{
+	public void cancelGame() throws InterruptedException, IOException
+	{
+		System.setProperty("webdriver.gecko.driver","geckodriver.exe");
+		WebDriver driver=new FirefoxDriver();
+		driver.manage().window().maximize();
+		FileInputStream fileStream=new FileInputStream(new File("cancelGame.xlsx"));
+		XSSFWorkbook workBook=new XSSFWorkbook(fileStream);
+		XSSFSheet sheet=workBook.getSheetAt(0);
+		int numOfRows=sheet.getLastRowNum();
+		String name,password;
+		String courseName,cancelGame,gameName;
+		for(int row=0;row<numOfRows;row++)
+		{
+			driver.findElement(By.id("join"));
+			Thread.sleep(2000);
+			driver.findElement(By.id("signIn")).click();
+			Thread.sleep(2000);
+			name=sheet.getRow(row).getCell(0).getStringCellValue();
+			password=sheet.getRow(row).getCell(1).getStringCellValue();
+			driver.findElement(By.id("userName")).sendKeys(name);
+			driver.findElement(By.id("password")).sendKeys(password);
+			driver.findElement(By.id("SignIn")).click();
+			
+			Thread.sleep(5000);
+			courseName=sheet.getRow(row).getCell(3).getStringCellValue();
+			
+			driver.findElement(By.id(courseName)).click();
+			Thread.sleep(5000);
+			
+			cancelGame=sheet.getRow(row).getCell(2).getStringCellValue();
+			
+			if(cancelGame.equals("cancel"))
+			{
+				gameName=sheet.getRow(row).getCell(4).getStringCellValue();
+				
+				driver.findElement(By.name(gameName)).click();
+				Thread.sleep(5000);
+				driver.findElement(By.id("Menu")).click();
+				Thread.sleep(2000);
+				driver.findElement(By.id("cancelGame")).click();
+				Thread.sleep(1000);
+			}
+			
+			Thread.sleep(3000);
+			driver.findElement(By.id("signOut")).click();
+			Thread.sleep(3000);
+		}
+		
+		fileStream.close();
+		workBook.close();
+		driver.close();
+		
+	}
+
+}
---
+++ Phase 3/ST-COMM-Testing-phase3/src/test/java/automatedTesting/CopyGame.java
@@ -0,0 +1,92 @@
+package automatedTesting;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class CopyGame
+{
+	public void copyGame() throws IOException, InterruptedException
+	{
+		System.setProperty("webdriver.gecko.driver","geckodriver.exe");
+		WebDriver driver=new FirefoxDriver();
+		driver.manage().window().maximize();
+		FileInputStream fileStream=new FileInputStream(new File("cancelGame.xlsx"));
+		XSSFWorkbook workBook=new XSSFWorkbook(fileStream);
+		XSSFSheet sheet=workBook.getSheetAt(0);
+		int numOfRows=sheet.getLastRowNum();
+		driver.findElement(By.id("join"));
+		Thread.sleep(2000);
+		driver.findElement(By.id("signIn")).click();
+		Thread.sleep(2000);
+		String name=sheet.getRow(0).getCell(0).getStringCellValue();
+		String password=sheet.getRow(0).getCell(1).getStringCellValue();
+		
+		driver.findElement(By.id("userName")).sendKeys(name);
+		driver.findElement(By.id("password")).sendKeys(password);
+		driver.findElement(By.id("SignIn")).click();
+		Thread.sleep(5000);
+		
+		for(int row=1;row<numOfRows-1;row++)
+		{
+			String courseName=sheet.getRow(row).getCell(0).getStringCellValue();
+			driver.findElement(By.id(courseName)).click();
+			
+			Thread.sleep(5000);
+			String gameName=sheet.getRow(row).getCell(1).getStringCellValue();
+			driver.findElement(By.name(gameName)).click();
+			Thread.sleep(5000);
+			driver.findElement(By.id("Menu")).click();
+			Thread.sleep(2000);
+			driver.findElement(By.id("copyGame")).click();
+			Thread.sleep(4000);
+			
+			String newCourseName=sheet.getRow(row).getCell(2).getStringCellValue();
+			if(newCourseName==null)newCourseName="";
+			String newGameName=sheet.getRow(row).getCell(3).getStringCellValue();
+			if(newGameName==null)newGameName="";
+			
+			driver.findElement(By.id("newCourseName")).sendKeys(newCourseName);
+			driver.findElement(By.id("newGameName")).sendKeys(newGameName);
+			driver.findElement(By.id("performCopy")).click();
+			Thread.sleep(2000);
+			driver.findElement(By.id("cancelCopy")).click();
+			Thread.sleep(2000);
+			driver.findElement(By.id("home")).click();
+			Thread.sleep(2000);
+		}
+		
+		Thread.sleep(3000);
+		driver.findElement(By.id("signOut")).click();
+		Thread.sleep(3000);
+		
+		driver.findElement(By.id("join"));
+		Thread.sleep(2000);
+		driver.findElement(By.id("signIn")).click();
+		Thread.sleep(2000);
+		String SName=sheet.getRow(5).getCell(0).getStringCellValue();
+		String SPassword=sheet.getRow(5).getCell(1).getStringCellValue();
+		
+		driver.findElement(By.id("userName")).sendKeys(SName);
+		driver.findElement(By.id("password")).sendKeys(SPassword);
+		driver.findElement(By.id("SignIn")).click();
+		Thread.sleep(5000);
+		
+		String CourseName=sheet.getRow(5).getCell(3).getStringCellValue();
+		driver.findElement(By.id(CourseName)).click();
+		
+		Thread.sleep(3000);
+		driver.findElement(By.id("signOut")).click();
+		Thread.sleep(3000);
+		
+		
+		fileStream.close();
+		workBook.close();
+		driver.close();
+	}
+}
